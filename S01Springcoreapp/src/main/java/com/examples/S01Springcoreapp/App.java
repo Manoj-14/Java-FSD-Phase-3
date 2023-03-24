b14dd05f185a5877c756501974ba370d9a946bd4
Manoj M <manumadhu1425@gmail.com>
2023-03-24 11:32:20 +0530

    First Spring App

--- S01Springcoreapp/src/main/java/com/examples/S01Springcoreapp/App.java
+++ S01Springcoreapp/src/main/java/com/examples/S01Springcoreapp/App.java
@@ -1,14 +1,20 @@
 package com.examples.S01Springcoreapp;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Employee emp = new Employee();
-        emp.setId(1);
-        emp.setName("Tom");
-        
-        System.out.println(emp.toString());
-    }
+public class App {
+	public static void main(String[] args) {
+//        Employee emp = new Employee();
+//        emp.setId(1);
+//        emp.setName("Tom");
+//        
+//        System.out.println(emp.toString());
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/examples/S01Springcoreapp/springconfig.xml");
+		Employee emp = context.getBean(Employee.class);
+		emp.setId(1);
+		emp.setName("Tom");
+		System.out.println(emp.toString());
+	}
 }
