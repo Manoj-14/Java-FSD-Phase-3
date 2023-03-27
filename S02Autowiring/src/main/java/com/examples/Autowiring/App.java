8b7b582dda6191a8baed82faf7d40dd5cc1968e8
Manoj M <manumadhu1425@gmail.com>
2023-03-27 11:53:40 +0530

    Autuwiring

--- S02Autowiring/src/main/java/com/examples/Autowiring/App.java
+++ S02Autowiring/src/main/java/com/examples/Autowiring/App.java
@@ -6,13 +6,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
 	public static void main(String[] args) {
 		demo1();
-
 	}
 	
 	private static void demo1() {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/examples/Autowiring/springconfig.xml");
-		Employee emp = (Employee)context.getBean("emp1");
-		System.out.println(emp);
+		Employee emp1 = (Employee)context.getBean("emp1");
+		System.out.println(emp1);
+		Employee emp2 = (Employee)context.getBean("emp2");
+		System.out.println(emp2);
 	}
 
 }
