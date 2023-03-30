825db0aacb44a7211514164808afc43973e2a8ad
Manoj M <manumadhu1425@gmail.com>
2023-03-30 16:56:00 +0530

    OSL Projects deleted

--- 02OSLAOP/src/main/java/com/example/OSLAOP/aop/TracibleService.java
+++
@@ -1,7 +0,0 @@
-package com.example.OSLAOP.aop;
-
-public class TracibleService {
-	public void hello(String subject) {
-		System.out.println(subject);
-	}
-}
--- 03OSLTransactionManagement/src/main/java/com/example/OSLTransactionManagement/tx/BusinessService.java
+++
@@ -1,5 +0,0 @@
-package com.example.OSLTransactionManagement.tx;
-
-public interface BusinessService {
-	void doBusiness();
-}
