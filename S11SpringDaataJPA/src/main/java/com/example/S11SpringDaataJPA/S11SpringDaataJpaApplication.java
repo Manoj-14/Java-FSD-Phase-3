3cbcc3b99cf42b6e610da7d59735c695e386cba5
Manoj M <manumadhu1425@gmail.com>
2023-03-31 11:16:05 +0530

    Spring Data JPA Project SetUP

---
+++ S11SpringDaataJPA/src/main/java/com/example/S11SpringDaataJPA/S11SpringDaataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.S11SpringDaataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S11SpringDaataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S11SpringDaataJpaApplication.class, args);
+	}
+
+}
---
+++ S11SpringDaataJPA/src/test/java/com/example/S11SpringDaataJPA/S11SpringDaataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.S11SpringDaataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S11SpringDaataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
