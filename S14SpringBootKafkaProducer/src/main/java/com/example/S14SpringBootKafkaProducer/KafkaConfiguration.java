2a3d89260d932c1c6a9002569cd538c2743a5c2f
Manoj M <manumadhu1425@gmail.com>
2023-04-04 16:49:10 +0530

    Kafka producer

--- S14SpringBootKafkaProducer/src/main/java/com/example/S14SpringBootKafkaProducer/KafkaConfiguration.java
+++ S14SpringBootKafkaProducer/src/main/java/com/example/S14SpringBootKafkaProducer/KafkaConfiguration.java
@@ -3,14 +3,16 @@ package com.example.S14SpringBootKafkaProducer;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaConfiguration {
@@ -19,14 +21,20 @@ public class KafkaConfiguration {
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> props = new HashMap<>();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
 		return new DefaultKafkaProducerFactory<>(props);
 	}
-	
+
 	@Bean
-	public KafkaTemplate<String, String> kafkaTemplate(){
+	public KafkaTemplate<String, String> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
 	}
+
+	@Bean
+	public NewTopic topicExample() {
+		return TopicBuilder.name("my-first-kafka-topic").partitions(6).replicas(3).build();
+	}
+
 }
---
+++ S14SpringBootKafkaProducer/src/main/java/com/example/S14SpringBootKafkaProducer/KafkaProducerController.java
@@ -0,0 +1,21 @@
+package com.example.S14SpringBootKafkaProducer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/send")
+public class KafkaProducerController {
+	
+	@Autowired
+	KafkaTemplate<String, String> kafkaTemplate;
+	
+	@RequestMapping("/{name}")
+	public void sendToKafka(@PathVariable("name") String name) {
+		kafkaTemplate.send("my-first-kafka-topic", name);
+	}
+}
